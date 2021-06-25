b6c71230ec78948b3121e2ad1696357ddf5c19ac
Greg Kuhlmann <greg@sumatra.ai>
2021-06-25 17:04:00 -0500

    added scowl lexer

---
+++ pygments/lexers/scowl.py
@@ -0,0 +1,48 @@
+from pygments.lexer import RegexLexer, bygroups, words
+from pygments.token import *
+
+
+class ScowlLexer(RegexLexer):
+    name = "Scowl"
+    aliases = ["scowl"]
+    filenames = ["*.scowl"]
+
+    tokens = {
+        'root': [
+            (r'\n', Text),
+            (r'\s+', Text),
+            (r'\\\n', Text),  # line continuations
+            (r'--(.*?)\n', Comment.Single),
+            (r'/(\\\n)?[*](.|\n)*?[*](\\\n)?/', Comment.Multiline),
+            (r'(event)\b', Keyword.Namespace),
+            (words((
+                'as', 'by', 'exclusive', 'default', 'when', 'then', 'contains', 'in', 'is', 'not', 'and', 'or',
+                'where', 'last', 'after', 'because'), suffix=r'\b'),
+             Keyword),
+            (r'(true|false|null|second|seconds|minute|minutes|hour|hours|day|days|week|weeks)\b', Keyword.Constant),
+            (words(('int', 'float', 'string',
+                    'bool', 'time'), suffix=r'\b'),
+             Keyword.Type),
+            # float_lit
+            (r'\d+(\.\d+[eE][+\-]?\d+|'
+             r'\.\d*|[eE][+\-]?\d+)', Number.Float),
+            (r'\.\d+([eE][+\-]?\d+)?', Number.Float),
+            # int_lit
+            (r'(0|[1-9][0-9]*)', Number.Integer),
+            # JSONPath string
+            (r'\$[^\s]*', String.Other),
+            # StringLiteral
+            (r'"(\\\\|\\"|[^"])*"', String.Double),
+            (r"'(\\\\|\\'|[^'])*'", String.Single),
+            # Tokens
+            (r'(<=|>=|='
+             r'|!=|//|:=|[+\-*/%])', Operator),
+            (r'[|^<>=!()\[\]{}.,;:]', Punctuation),
+            # identifier
+            (r'[A-Z]\w*', Name.Function),
+            (r'[^\W\d]\w*', Name.Other),
+        ]
+    }
+
+
+__all__ = ['ScowlLexer']
